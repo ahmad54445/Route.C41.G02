1cbe2f7c0a4b858f5513485c8ab5edc389e2d0de
Mo5tar <Mo5tar@DESKTOP-NC91KAR>
2024-03-23 01:34:47 +0200

    DAL  For Department Module

---
+++ Route.C41.G02.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Route.C41.G02.DAL.Data.Configurations;
+using Route.C41.G02.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.C41.G02.DAL.Data
+{
+    internal class ApplicationDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer("Server = .; Database=MVCAppliction; Trusted_Connection =True, MultipleActiveResultSets = false ");
+        //MultipleActiveResultSets => defulate = false
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration<Department>(new DepartmentConfigurations());
+
+            //Useing Refaltion Metdata
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly:Assembly.GetExecutingAssembly());
+        }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ Route.C41.G02.DAL/Data/Configurations/DepartmentConfigurations.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Route.C41.G02.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Route.C41.G02.DAL.Data.Configurations
+{
+    internal class DepartmentConfigurations : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            //Fluent APi for "Department" Domain
+            builder.Property(D=>D.Id).UseIdentityColumn(10,10);
+            builder.Property(D=>D.Code).HasColumnType("varChar").HasMaxLength(50).IsRequired();
+            builder.Property(D=>D.Name).HasColumnType("varChar").HasMaxLength(50).IsRequired();
+        }
+    }
+}
